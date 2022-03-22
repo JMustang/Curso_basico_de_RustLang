8ee9a1e46f6f7f8454ed7a5b2a79a5772391b934
JMustang <eecfredes@gmail.com>
2022-03-21 21:20:00 -0300

    projeto calculadora - step four - Rodando o codigo

--- calculadora/src/main.rs
+++ calculadora/src/main.rs
@@ -1,3 +1,13 @@
+use std::env::{args, Args};
+
 fn main() {
-    println!("Hello, world!");
+    let mut args: Args = args();
+
+    // O primeiro argumento é a localização do binário compilado, então pule-o
+    let first: String = args.nth(1).unwrap();
+    // Depois de acessar o segundo argumento, o próximo elemento do iterador se torna o primeiro
+    let operator: String = args.nth(0).unwrap();
+    let second: String = args.nth(0).unwrap();
+
+    println!("{} {} {}", first, operator, second);
 }
---
+++ calculadora/target/debug/deps/calculadora-5ec33be35e43b2d6.d
@@ -0,0 +1,5 @@
+/home/junior/Code Projects/Rust-Lang/Curso_basico_de_RustLang/calculadora/target/debug/deps/calculadora-5ec33be35e43b2d6: src/main.rs
+
+/home/junior/Code Projects/Rust-Lang/Curso_basico_de_RustLang/calculadora/target/debug/deps/calculadora-5ec33be35e43b2d6.d: src/main.rs
+
+src/main.rs:
